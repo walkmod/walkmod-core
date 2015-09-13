2c45045c663bf49b04fa7ed15b3ced2b2a77c3c1
rpau <rpau@ac.upc.edu>
2015-09-13 12:59:22 +0200

    adding getters to the Options class to write less code to read options

--- src/main/java/org/walkmod/Options.java
+++ src/main/java/org/walkmod/Options.java
@@ -11,93 +11,151 @@ import java.util.*;
  */
 public class Options {
 
-    /**
-     * (Boolean) Disables/enables remote fetching of plugins
-     */
-    public static final String OFFLINE = "offline";
-    /**
-     * (Boolean) Disables/enables info messages in the console
-     */
-    public static final String VERBOSE = "verbose";
-    /**
-     * (Boolean) Disables/enables error messages in the console
-     */
-    public static final String PRINT_ERRORS = "print_errors";
-    /**
-     * (Boolean) Disables/enables the capture of original exceptions (requires verbose = true)
-     */
-    public static final String THROW_EXCEPTION = "throw_exception";
-    /**
-     * (List&lt;String&gt;) Overwrites the include rules in the chain's reader
-     */
-    public static final String INCLUDES = "includes";
-    /**
-     * (List&lt;String&gt;) Overwrites the exclude rules in the chain's reader
-     */
-    public static final String EXCLUDES = "excludes";
+	/**
+	 * (Boolean) Disables/enables remote fetching of plugins
+	 */
+	public static final String OFFLINE = "offline";
+	/**
+	 * (Boolean) Disables/enables info messages in the console
+	 */
+	public static final String VERBOSE = "verbose";
+	/**
+	 * (Boolean) Disables/enables error messages in the console
+	 */
+	public static final String PRINT_ERRORS = "print_errors";
+	/**
+	 * (Boolean) Disables/enables the capture of original exceptions (requires
+	 * verbose = true)
+	 */
+	public static final String THROW_EXCEPTION = "throw_exception";
+	/**
+	 * (List&lt;String&gt;) Overwrites the include rules in the chain's reader
+	 */
+	public static final String INCLUDES = "includes";
+	/**
+	 * (List&lt;String&gt;) Overwrites the exclude rules in the chain's reader
+	 */
+	public static final String EXCLUDES = "excludes";
 
-    /**
-     * Stored options
-     */
-    private Map<String, Object> options = new HashMap<String, Object>();
+	/**
+	 * Stored options
+	 */
+	private Map<String, Object> options = new HashMap<String, Object>();
 
-    /**
-     * Creates an empty set of options
-     */
-    public Options() {
-    }
+	/**
+	 * Creates an empty set of options
+	 */
+	public Options() {
+		this(null);
+	}
 
-    /**
-     * Creates a set of options with initialized values
-     *
-     * @param options Already initialized options
-     */
-    public Options(Map<String, Object> options) {
-        this.options.putAll(options);
-    }
+	/**
+	 * Creates a set of options with initialized values
+	 *
+	 * @param options
+	 *            Already initialized options
+	 */
+	public Options(Map<String, Object> options) {
+		if (options != null) {
+			this.options.putAll(options);
+			if (!options.containsKey(Options.OFFLINE))
+				setOffline(false);
+			if (!options.containsKey(Options.VERBOSE))
+				setVerbose(true);
+			if (!options.containsKey(Options.PRINT_ERRORS))
+				setPrintErrors(false);
+			if (!options.containsKey(Options.THROW_EXCEPTION))
+				setThrowException(false);
+		}
+		else{
+			setOffline(false);
+			setVerbose(true);
+			setPrintErrors(false);
+			setThrowException(false);
+		}
+	}
 
-    public void setOffline(boolean offline) {
-        this.options.put(OFFLINE, Boolean.valueOf(offline));
-    }
+	public void setOffline(boolean offline) {
+		this.options.put(OFFLINE, Boolean.valueOf(offline));
+	}
 
-    public void setVerbose(boolean verbose) {
-        this.options.put(VERBOSE, Boolean.valueOf(verbose));
-    }
+	public boolean isOffline() {
+		Object value = this.options.get(OFFLINE);
+		return value != null && (Boolean) value;
 
-    public void setPrintErrors(boolean printErrors) {
-        this.options.put(PRINT_ERRORS, Boolean.valueOf(printErrors));
-    }
+	}
 
-    public void setThrowException(boolean throwException) {
-        this.options.put(THROW_EXCEPTION, Boolean.valueOf(throwException));
-    }
+	public void setVerbose(boolean verbose) {
+		this.options.put(VERBOSE, Boolean.valueOf(verbose));
+	}
 
-    public void setIncludes(String... includes) {
+	public boolean isVerbose() {
+		Object value = this.options.get(VERBOSE);
+		return value != null && (Boolean) value;
+	}
 
-        if (!this.options.containsKey(INCLUDES)) {
-            this.options.put(INCLUDES, new ArrayList<Object>());
-        }
+	public void setPrintErrors(boolean printErrors) {
+		this.options.put(PRINT_ERRORS, Boolean.valueOf(printErrors));
+	}
 
-        List<Object> allIncludes = (List<Object>) this.options.get(INCLUDES);
-        allIncludes.addAll(Arrays.asList(includes));
-    }
+	public boolean isPrintErrors() {
+		Object value = this.options.get(PRINT_ERRORS);
+		return value != null && (Boolean) value;
+	}
 
-    public void setExcludes(String... excludes) {
+	public void setThrowException(boolean throwException) {
+		this.options.put(THROW_EXCEPTION, Boolean.valueOf(throwException));
+	}
 
-        if (!this.options.containsKey(EXCLUDES)) {
-            this.options.put(EXCLUDES, new ArrayList<Object>());
-        }
+	public boolean isThrowException() {
+		Object value = this.options.get(THROW_EXCEPTION);
+		return value != null && (Boolean) value;
+	}
 
-        List<Object> allIncludes = (List<Object>) this.options.get(EXCLUDES);
-        allIncludes.addAll(Arrays.asList(excludes));
-    }
+	@SuppressWarnings("unchecked")
+	public void setIncludes(String... includes) {
 
-    /**
-     * Returns the stored options as a Map&lt;String,Object&gt;
-     *
-     * @return map with options
-     */
-    public Map<String, Object> asMap() {
-        return this.options;
-    }
+		if (!this.options.containsKey(INCLUDES)) {
+			this.options.put(INCLUDES, new ArrayList<Object>());
+		}
+
+		List<Object> allIncludes = (List<Object>) this.options.get(INCLUDES);
+		allIncludes.addAll(Arrays.asList(includes));
+	}
+
+	@SuppressWarnings("unchecked")
+	public void setExcludes(String... excludes) {
+
+		if (!this.options.containsKey(EXCLUDES)) {
+			this.options.put(EXCLUDES, new ArrayList<Object>());
+		}
+
+		List<Object> allIncludes = (List<Object>) this.options.get(EXCLUDES);
+		allIncludes.addAll(Arrays.asList(excludes));
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<String> getExcludes() {
+		if (options.containsKey(EXCLUDES)) {
+			return (List<String>) options.get(EXCLUDES);
+		}
+		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<String> getIncludes() {
+		if (options.containsKey(INCLUDES)) {
+			return (List<String>) options.get(INCLUDES);
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the stored options as a Map&lt;String,Object&gt;
+	 *
+	 * @return map with options
+	 */
+	public Map<String, Object> asMap() {
+		return this.options;
+	}
 }
--- src/main/java/org/walkmod/OptionsBuilder.java
+++ src/main/java/org/walkmod/OptionsBuilder.java
@@ -5,156 +5,161 @@ import java.util.Map;
 /**
  * Helper class to create Walkmod options map.
  *
- * Usage: OptionsBuilder.options.offline(true).includes("src/main","src/test").asMap()
+ * Usage:
+ * OptionsBuilder.options.offline(true).includes("src/main","src/test").asMap()
  *
  * @author abelsromero
  */
 public class OptionsBuilder {
 
-    /**
-     * Options instance
-     */
-    private Options options;
+	/**
+	 * Options instance
+	 */
+	private Options options;
 
-    /**
-     * Creates an OptionBuilder with the default options
-     */
-    private OptionsBuilder() {
-        options = new Options();
-        options.setOffline(false);
-        options.setVerbose(true);
-        options.setPrintErrors(false);
-        options.setThrowException(false);
-    }
+	/**
+	 * Creates an OptionBuilder with the default options
+	 */
+	private OptionsBuilder() {
+		options = new Options();
 
-    /**
-     * Creates an OptionBuilder with the initialization options
-     */
-    private OptionsBuilder(Map<String, Object> options) {
-        this.options = new Options(options);
-        // Check if default values are set
-        if (!options.containsKey(Options.OFFLINE))
-            this.options.setOffline(false);
-        if (!options.containsKey(Options.VERBOSE))
-            this.options.setVerbose(true);
-        if (!options.containsKey(Options.PRINT_ERRORS))
-            this.options.setPrintErrors(false);
-        if (!options.containsKey(Options.THROW_EXCEPTION))
-            this.options.setThrowException(false);
-    }
+	}
 
-    /**
-     * Creates an OptionBuilder instance with the default options:
-     *
-     * <ul>
-     *   <li>offline = false
-     *   <li>verbose = true
-     *   <li>printErrors = false
-     *   <li>throwException = false
-     * </ul>
-     *
-     * @return options builder instance
-     */
-    public static OptionsBuilder options() {
-        return new OptionsBuilder();
-    }
+	/**
+	 * Creates an OptionBuilder with the initialization options
+	 */
+	private OptionsBuilder(Map<String, Object> options) {
+		this.options = new Options(options);
 
-    /**
-     * Creates an OptionBuilder instance with the default options:
-     *
-     * @param options Map of starting options. See {@link Options} for available
-     *                keys
-     *
-     * @return options builder instance
-     */
-    public static OptionsBuilder options(Map<String, Object> options) {
-        return new OptionsBuilder(options);
-    }
+	}
 
-    /**
-     * Sets the offline option
-     *
-     * @param offline true to disable resolution of plugin from remote repositories
-     * @return updated OptionBuilder instance
-     * @see Options#OFFLINE
-     */
-    public OptionsBuilder offline(boolean offline) {
-        options.setOffline(offline);
-        return this;
-    }
+	/**
+	 * Creates an OptionBuilder instance with the default options:
+	 *
+	 * <ul>
+	 * <li>offline = false
+	 * <li>verbose = true
+	 * <li>printErrors = false
+	 * <li>throwException = false
+	 * </ul>
+	 *
+	 * @return options builder instance
+	 */
+	public static OptionsBuilder options() {
+		return new OptionsBuilder();
+	}
 
-    /**
-     * Sets the verbose option
-     *
-     * @param verbose true to enable info messages in the console
-     * @return updated OptionBuilder instance
-     *
-     * @see Options#VERBOSE
-     */
-    public OptionsBuilder verbose(boolean verbose) {
-        options.setVerbose(verbose);
-        return this;
-    }
+	/**
+	 * Creates an OptionBuilder instance with the default options:
+	 *
+	 * @param options
+	 *            Map of starting options. See {@link Options} for available
+	 *            keys
+	 *
+	 * @return options builder instance
+	 */
+	public static OptionsBuilder options(Map<String, Object> options) {
+		return new OptionsBuilder(options);
+	}
 
-    /**
-     * Sets the printErrors option
-     *
-     * @param printErrors true to enable error messages in the console
-     * @return updated OptionBuilder instance
-     *
-     * @see Options#PRINT_ERRORS
-     */
-    public OptionsBuilder printErrors(boolean printErrors) {
-        options.setPrintErrors(printErrors);
-        return this;
-    }
+	/**
+	 * Sets the offline option
+	 *
+	 * @param offline
+	 *            true to disable resolution of plugin from remote repositories
+	 * @return updated OptionBuilder instance
+	 * @see Options#OFFLINE
+	 */
+	public OptionsBuilder offline(boolean offline) {
+		options.setOffline(offline);
+		return this;
+	}
 
-    /**
-     * Sets the printErrors option
-     *
-     * @param throwException true to enable exception throwing
-     * @return updated OptionBuilder instance
-     *
-     * @see Options#PRINT_ERRORS
-     */
-    public OptionsBuilder throwException(boolean throwException) {
-        options.setThrowException(throwException);
-        return this;
-    }
+	/**
+	 * Sets the verbose option
+	 *
+	 * @param verbose
+	 *            true to enable info messages in the console
+	 * @return updated OptionBuilder instance
+	 *
+	 * @see Options#VERBOSE
+	 */
+	public OptionsBuilder verbose(boolean verbose) {
+		options.setVerbose(verbose);
+		return this;
+	}
 
-    /**
-     * Sets the includes option
-     *
-     * @param includes List of included paths
-     * @return updated OptionBuilder instance
-     *
-     * @see Options#INCLUDES
-     */
-    public OptionsBuilder includes(String... includes) {
-        options.setIncludes(includes);
-        return this;
-    }
+	/**
+	 * Sets the printErrors option
+	 *
+	 * @param printErrors
+	 *            true to enable error messages in the console
+	 * @return updated OptionBuilder instance
+	 *
+	 * @see Options#PRINT_ERRORS
+	 */
+	public OptionsBuilder printErrors(boolean printErrors) {
+		options.setPrintErrors(printErrors);
+		return this;
+	}
 
-    /**
-     * Sets the excludes
-     *
-     * @param excludes List of excluded paths
-     * @return updated OptionBuilder instance
-     *
-     * @see Options#EXCLUDES
-     */
-    public OptionsBuilder excludes(String... excludes) {
-        options.setExcludes(excludes);
-        return this;
-    }
+	/**
+	 * Sets the printErrors option
+	 *
+	 * @param throwException
+	 *            true to enable exception throwing
+	 * @return updated OptionBuilder instance
+	 *
+	 * @see Options#PRINT_ERRORS
+	 */
+	public OptionsBuilder throwException(boolean throwException) {
+		options.setThrowException(throwException);
+		return this;
+	}
 
-    /**
-     * Returns the stored options as a Map&lt;String,Object&gt;
-     *
-     * @return map with options
-     */
-    public Map<String, Object> asMap() {
-        return options.asMap();
-    }
+	/**
+	 * Sets the includes option
+	 *
+	 * @param includes
+	 *            List of included paths
+	 * @return updated OptionBuilder instance
+	 *
+	 * @see Options#INCLUDES
+	 */
+	public OptionsBuilder includes(String... includes) {
+		options.setIncludes(includes);
+		return this;
+	}
+
+	/**
+	 * Sets the excludes
+	 *
+	 * @param excludes
+	 *            List of excluded paths
+	 * @return updated OptionBuilder instance
+	 *
+	 * @see Options#EXCLUDES
+	 */
+	public OptionsBuilder excludes(String... excludes) {
+		options.setExcludes(excludes);
+		return this;
+	}
+
+
+	/**
+	 * 
+	 * @return the current built options
+	 */
+	public Options build(){
+		return options;
+	}
+	/**
+	 * Returns the stored options as a Map&lt;String,Object&gt;
+	 *
+	 * @return map with options
+	 */
+	public Map<String, Object> asMap() {
+		return options.asMap();
+	}
 
 }
