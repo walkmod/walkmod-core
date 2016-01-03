dffb2d74cae1c6fc74ab26ec530aeeb6d989b9b8
rpau <rpau@ac.upc.edu>
2016-01-03 16:11:10 +0100

    filtering parameters config per type

--- src/main/java/org/walkmod/commands/InspectCommand.java
+++ src/main/java/org/walkmod/commands/InspectCommand.java
@@ -15,6 +15,7 @@
  along with Walkmod.  If not, see <http://www.gnu.org/licenses/>.*/
 package org.walkmod.commands;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.walkmod.OptionsBuilder;
@@ -32,69 +33,71 @@ import de.vandermeer.asciitable.v2.V2_AsciiTable;
 @Parameters(separators = "=", commandDescription = "Prints the components of a walkmod plugin")
 public class InspectCommand implements Command, AsciiTableAware {
 
-	@Parameter(names = "--help", help = true, hidden = true)
-	private boolean help;
+   @Parameter(names = "--help", help = true, hidden = true)
+   private boolean help;
 
-	private JCommander command;
+   private JCommander command;
 
-	@Parameter(description = "The plugin id to inspect (e.g imports-cleaner)")
-	private List<String> pluginId;
+   @Parameter(description = "The plugin id to inspect (e.g imports-cleaner)")
+   private List<String> pluginId;
 
-	@Parameter(names = "--offline", description = "Resolves the walkmod plugins and their dependencies in offline mode")
-	private boolean offline = false;
+   @Parameter(names = "--offline", description = "Resolves the walkmod plugins and their dependencies in offline mode")
+   private boolean offline = false;
 
-	private V2_AsciiTable at = null;
+   private V2_AsciiTable at = null;
 
-	public InspectCommand(JCommander command) {
-		this.command = command;
-	}
+   public InspectCommand(JCommander command) {
+      this.command = command;
+   }
 
-	@Override
-	public void execute() throws Exception {
-		if (help) {
-			command.usage("inspect");
-		} else {
-			WalkModFacade facade = new WalkModFacade(OptionsBuilder.options().offline(offline));
-			List<BeanDefinition> beans = facade.inspectPlugin(new PluginConfigImpl(pluginId.get(0)));
-			if (beans != null) {
+   @Override
+   public void execute() throws Exception {
+      if (help) {
+         command.usage("inspect");
+      } else {
+         WalkModFacade facade = new WalkModFacade(OptionsBuilder.options().offline(offline));
+         List<BeanDefinition> beans = facade.inspectPlugin(new PluginConfigImpl(pluginId.get(0)));
 
-				at = new V2_AsciiTable();
-				at.addRule();
-				at.addRow("TYPE NAME (ID)", "CATEGORY","PROPERTIES", "DESCRIPTION");
-				at.addStrongRule();
+         List<String> validTypesList = Arrays.asList("String", "JSONObject", "JSONArray");
+         if (beans != null) {
 
-				for (BeanDefinition bean : beans) {
-					List<PropertyDefinition> properties = bean.getProperties();
-					if(properties == null || properties.isEmpty()){
-						at.addRow(bean.getType(), bean.getCategory(), "", bean.getDescription());
-					}
-					else{
-						int i = 0;
-						for(PropertyDefinition pd : properties){
-							String label = pd.getName()+":"+pd.getType();
-							if(pd.getDefaultValue() != null && pd.getDefaultValue().length() != 0){
-								label = label +" ("+pd.getDefaultValue()+")";
-							}
-							if(i == 0){
-								at.addRow(bean.getType(), bean.getCategory(), label, bean.getDescription());
-							}
-							else{
-								at.addRow("", "", label, "");
-							}
-							i++;
-						}
-					}
-					at.addRule();
+            at = new V2_AsciiTable();
+            at.addRule();
+            at.addRow("TYPE NAME (ID)", "CATEGORY", "PROPERTIES", "DESCRIPTION");
+            at.addStrongRule();
 
-				}
-				
-			}
-		}
-	}
+            for (BeanDefinition bean : beans) {
+               List<PropertyDefinition> properties = bean.getProperties();
+               if (properties == null || properties.isEmpty()) {
+                  at.addRow(bean.getType(), bean.getCategory(), "", bean.getDescription());
+               } else {
+                  int i = 0;
+                  for (PropertyDefinition pd : properties) {
+                     if (validTypesList.contains(pd.getType())) {
+                        String label = pd.getName() + ":" + pd.getType();
+                        if (pd.getDefaultValue() != null && pd.getDefaultValue().length() != 0) {
+                           label = label + " (" + pd.getDefaultValue() + ")";
+                        }
+                        if (i == 0) {
+                           at.addRow(bean.getType(), bean.getCategory(), label, bean.getDescription());
+                        } else {
+                           at.addRow("", "", label, "");
+                        }
+                        i++;
+                     }
+                  }
+               }
+               at.addRule();
 
-	@Override
-	public V2_AsciiTable getTable() {
-		return at;
-	}
+            }
+
+         }
+      }
+   }
+
+   @Override
+   public V2_AsciiTable getTable() {
+      return at;
+   }
 
 }
